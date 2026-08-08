5289d014972a923f8cb969d72abef515d4abe559
Victor Vaz <victorvaz06@gmail.com>
2026-08-07 22:44:36 -0300

    SABOR springboot

---
+++ back-end/ecommerce-pedidos-comprai/src/main/java/com/ecommerce/pedidos/comprai/App.java
@@ -0,0 +1,10 @@
+package com.ecommerce.pedidos..comprai;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
